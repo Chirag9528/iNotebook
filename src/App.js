229ffbdc4cdaa22cf46826487fe5f658778bfc8d
Chirag9528 <chiragvarshney9528@gmail.com>
2024-04-27 18:57:25 +0530

    basic set up for inotebook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-    <h1>This is iNotebook</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route element={<Home/>} exact path="/"></Route>  
+          <Route element={<About/>} exact path="/about"></Route>    
+        </Routes>
+      </Router>
     </>
   );
 }
